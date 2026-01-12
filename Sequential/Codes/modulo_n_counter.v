3fb920d82a83ae9be63c145f442979c4f005efa8
Chinmay Joshi <chinmay0508@gmail.com>
2026-01-12 15:58:50 +0530

    Added N-bit modulo counter

---
+++ Sequential/Codes/modulo_n_counter.v
@@ -0,0 +1,22 @@
+module modulo_n_counter (clk,rst,en,count,tc);
+    parameter  N = 10;
+    parameter integer WIDTH = (N > 1) ? $clog2(N) : 1;
+    input clk,rst,en;
+    output reg  [WIDTH-1:0]  count;
+    output tc;
+
+    assign tc = (count == N-1);
+
+    always @(posedge clk) 
+    begin
+        if (rst)
+            count <= '0;
+        else if (en) 
+        begin
+            if (count == N-1)
+                count <= '0;
+            else
+                count <= count + 1'b1;
+        end
+    end
+endmodule
--- Sequential/Codes/upDownCounterNbit.v
+++ Sequential/Codes/upDownCounterNbit.v
@@ -1,23 +1,25 @@
-module upDown_counter (clk,rst,en,mode,Q,tc_up,tc_down);
+module  (clk,rst,en,mode,Q,tc_up,tc_down);
     parameter N=8;
     input clk;
-    input rst;       // synchronous reset
-    input en;        // enable counting
-    input mode;      // 1 = count up, 0 = count down
+    input rst;    
+    input en;   
+    input mode;      // 1 = up, 0 = down
     output reg  [N-1:0] Q;
     output tc_up;    // terminal count for up
     output tc_down;  // terminal count for down
 
-assign tc_up   = (Q == {N{1'b1}}); // max value reached
-assign tc_down = (Q == {N{1'b0}}); // zero reached
-always @(posedge clk) begin
-    if (rst)
-        Q <= {N{1'b0}};
-    else if (en) begin
-        if (mode)
-            Q <= Q + 1'b1;    // count up
-        else
-            Q <= Q - 1'b1;    // count down
+    assign tc_up   = (Q == {N{1'b1}}); // max value reached
+    assign tc_down = (Q == {N{1'b0}}); // zero reached
+    always @(posedge clk) 
+    begin
+        if (rst)
+            Q <= '0;
+        else if (en) 
+        begin
+            if (mode)
+                Q <= Q + 1'b1;  
+            else
+                Q <= Q - 1'b1;   
+        end
     end
-end
 endmodule
